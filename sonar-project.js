aac5a837c1dbb5ac09324cb089ca4fbb1118085c
Gangadhar-HB <92389737+Gangadhar-HB@users.noreply.github.com>
2021-11-17 22:01:38 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://65.0.87.231:9000/',
+  serverUrl: 'http://3.110.135.63:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
