197b3d07b1dcda98d0a3c4b5860247e117fbb1df
Gangadhar-HB <92389737+Gangadhar-HB@users.noreply.github.com>
2022-06-08 21:57:57 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,13 +1,13 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.110.135.63:9000/',
+  serverUrl: 'http://65.1.130.85:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'admin',
-	    'sonar.password': 'sonarqube',
+	    'sonar.login': 'sonar',
+	    'sonar.password': 'sonar',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
